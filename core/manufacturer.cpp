57141f8fa061c39cfe8bb1b399a851c4cfabc024
BananaFanatics <adrien_janez@hotmail.com>
2012-07-28 17:37:00 +0300

    Update core/manufacturer.cpp

--- core/manufacturer.cpp
+++ core/manufacturer.cpp
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const QStringList Manufacturer::m_defaultNames =
     (QStringList()
+     << "Generic"
      << "Altera"
      << "Analog Devices"
      << "Atmel"
